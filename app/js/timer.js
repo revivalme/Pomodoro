0c06c2c8cff77a3b1d0603f82aa2e79409953c05
Danil <danik.tihiy@mail.ru>
2018-10-21 21:55:56 +0300

    Updated Timer object
    Added default property.
    Added start, stop and zeroBase methods.
    Changed getTime and setTime methods

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,22 +1,82 @@
 class Timer {
-  // Default ms = 25min
+  // Default time = 25min
   constructor(ms = 1500 * 1000) {
     this.value = new Date(ms);
     this.status = 0;
+    this.default = ms;
   }
 
   getTime() {
-    return {
-      minutes: this.value.getMinutes(),
-      seconds: this.value.getSeconds()
-    }
+    return `${this.zeroBase(this.value.getMinutes())} : ${this.zeroBase(this.value.getSeconds())}`;
   }
 
-  setTime(ms) {
+  setDefaultTime(ms) {
     this.value = new Date(ms);
+    this.default = ms;
   }
 
   cutTime(sec) {
-    this.value = new Date(this.getTime - sec * 1000);
+    this.value = new Date(this.value.getTime() - sec * 1000);
+  }
+
+  start() {
+    // If timer isn't active
+    if(!this.status) {
+      // Change status
+      this.status = 1;
+      // Change buttons text
+      startBtn.textContent = 'Pause';
+      stopBtn.textContent = 'Stop';
+
+      // Declare this for async func
+      const self = this;
+
+      // Async recursion func
+      setTimeout(function timerCycle() {
+        // If timer active
+        if(self.status) {
+          // If timer milliseconds > 0
+          if(self.value.getTime() > 0) {
+            // Subtract 1 second
+            self.cutTime(1);
+
+            // Update UI
+            ui.updateTimer(`${self.getTime()}`);
+            setTimeout(timerCycle, 1000);
+          } else {
+            // Time left - reset
+            self.reset();
+          }
+        }
+      }, 0);
+    } else {
+      // Change status
+      this.status = 0;
+      // Change buttons text
+      startBtn.textContent = 'Resume';
+      stopBtn.textContent = 'Done';
+    }
+  }
+
+  stop() {
+    // Reset
+    this.reset();
+  }
+
+  reset() {
+    // Change status
+    this.status = 0;
+    // Change buttons text
+    startBtn.textContent = 'Start';
+    stopBtn.textContent = 'Stop';
+    // Change timer value to default
+    this.value = new Date(this.default);
+    // Update UI
+    ui.updateTimer(`${this.getTime()}`);
+  }
+
+  // Pass minutes or seconds val
+  zeroBase(val) {
+    return val < 10 ? '0' + val : val;
   }
 }
