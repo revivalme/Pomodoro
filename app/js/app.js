d8d829f3dec03de9f8a1fc46acfcd4c954c82a04
Danil <danik.tihiy@mail.ru>
2018-10-22 19:30:32 +0300

    Implemented add new task

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,7 @@ const timer = new Timer;
 // DOM Elements
 const startBtn = document.querySelector('#btnStart');
 const stopBtn = document.querySelector('#btnStop');
+const form = document.querySelector('#form');
 
 // Events
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,3 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 startBtn.addEventListener('click', () => timer.start());
 stopBtn.addEventListener('click', () => timer.stop());
+form.addEventListener('submit', (e) => addTask(e));
+
+// Add task
+function addTask(e) {
+  const taskCategory = form.querySelector('#input-category');
+  const taskDescr = form.querySelector('#input-descr');
+
+  if(taskCategory.value !== '' && taskDescr.value !== '') {
+    // Get table element
+    const table = document.querySelector('#todoTable');
+    // Create task obj
+    const task = new Task(taskCategory.value, taskDescr.value);
+    // Put new task to todoTable
+    ui.putTask(ui.createTask(task), table);
+    // Clear inputs
+    taskCategory.value = '';
+    taskDescr.value = '';
+  } else {
+    // Show alert
+  }
+
+  // Prevent default event
+  e.preventDefault();
+}
