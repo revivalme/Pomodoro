afc98eee2d41b3a27b38d19a1405af1dc3d01043
AdelNorberg <darzemanov101@mail.com>
2018-10-23 18:55:22 +0300

    BackendEnv

---
+++ server/src/index.js
@@ -0,0 +1,48 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+import {connect} from "./database";
+// End file storage config
+
+const PORT = 3000;
+const app = express();
+app.server = http.createServer(app);
+
+
+app.use(morgan('dev'));
+
+
+app.use(cors({
+    exposedHeaders: "*"
+}));
+
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+
+app.set('root', __dirname);
+
+//Connect to the database.
+
+connect((err, db) => {
+
+    if(err){
+        console.log("An error connecting to the database", err);
+        throw (err);
+    }
+
+    app.db = db;
+    app.set('db', db);
+
+    app.server.listen(process.env.PORT || PORT, () => {
+        console.log(`App is running on port ${app.server.address().port}`);
+    });
+
+});
+
+
+
+export default app;
