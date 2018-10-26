eb34dd9ac515de9c1e1f9ba641315497850193cf
Danil <danik.tihiy@mail.ru>
2018-10-26 11:50:23 +0300

    Drag n drop

--- app/js/app.js
+++ app/js/app.js
@@ -23,6 +23,47 @@ todoTable.addEventListener('click', (e) => {
   }
 });
 
+// Drag n drop task events
+todoTable.querySelector('tbody').addEventListener('mousedown', (e) => {
+  // If event on TR element, then add attribute
+  if(e.target.parentElement.tagName === 'TR') {
+    e.target.parentElement.setAttribute('draggable', 'true');
+  }
+});
+todoTable.querySelector('tbody').addEventListener('mouseup', (e) => {
+  // If user mouseup instead drag, then remove attribute
+  if(e.target.parentElement.tagName === 'TR') {;
+    e.target.parentElement.removeAttribute('draggable');
+  }
+});
+todoTable.querySelector('tbody').addEventListener('dragenter', (e) => {
+  const currentTask = e.target.parentElement;
+  const dragEl = document.querySelector('tr[draggable=true');
+  if(currentTask.tagName === 'TR' && currentTask !== dragEl) {
+    // Compare offsetTop for good styling
+    dragEl.offsetTop < currentTask.offsetTop ? 
+    currentTask.style.borderBottom = '2px dotted #141414' :
+    currentTask.style.borderTop = '2px dotted #141414';
+  }
+});
+todoTable.querySelector('tbody').addEventListener('dragleave', (e) => e.target.parentElement.style.border = 0);
+todoTable.querySelector('tbody').addEventListener('dragover', (e) => e.preventDefault());
+todoTable.querySelector('tbody').addEventListener('drop', (e) => {
+  const currentTask = e.target.parentElement;
+  const dragEl = document.querySelector('tr[draggable=true');
+  if(currentTask.tagName === 'TR' && currentTask !== dragEl) {
+    currentTask.style.border = 0;
+    dragEl.removeAttribute('draggable');
+
+    const clone = dragEl.cloneNode(true);
+    dragEl.offsetTop < currentTask.offsetTop ? 
+    currentTask.after(clone) :
+    currentTask.before(clone);
+
+    dragEl.remove();
+  }
+});
+
 // Add task
 function addTask(e) {
   const taskCategory = form.querySelector('#input-category');
